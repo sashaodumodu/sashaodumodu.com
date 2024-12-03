013d9e405de13a42fb76ef0801d76deb78a20e7f
sashaodumodu <odumodusasha@gmail.com>
2024-12-03 11:03:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("hamburger").addEventListener("click", function () {
+    const menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("active");
+});
