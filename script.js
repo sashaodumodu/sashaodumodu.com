678c179478439dfb2b8cbfbbe13cdeb7e7ea467a
sashaodumodu <odumodusasha@gmail.com>
2024-12-03 22:54:19 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById("hamburger").addEventListener("click", function () {
-    const menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("active");
-});
